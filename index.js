9f7fcd3499a3d31eaa7ee1669a24124a0eef2d50
Itay Beladev <56035342+Itay212121@users.noreply.github.com>
2022-06-29 18:19:53 +0300

    Changed index.js
    changed the extension to return the page to what it was before the translation after 3 seconds

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 This function takes messages from the background, and replaces appearances of the text to the translated text
 */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    from = request.from;
-    translated = request.translated;
+    let baseHTML = document.body.innerHTML;
+    var from = request.from;
+    var translated = request.translated;
     document.body.innerHTML = document.body.innerHTML.replaceAll(from, translated);
+    setTimeout(() => {
+        document.body.innerHTML = baseHTML; //reloading the page to be the text it was before
+    }, 30000);
 });
